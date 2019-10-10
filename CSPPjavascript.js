320cade8d39f8154a6566165017c03b6b7f22c8c
Rattikarn <soyosss95@gmail.com>
2019-10-10 11:58:57 -0400

    Ui site html for sign up, log in, and user profile

--- CSPPjavascript.js
+++ CSPPjavascript.js
@@ -1,33 +1,5 @@
 
 //Onload  // DOM innerHTML stuff
-	window.onload = function() {
-
-
-		document.getElementById("universal-navbar").innerHTML =
-			 '<div class="container-fluid">'+
-				 '<nav>'+
-				    '<ul class="nav nav-fill">'+
-					  '<li class="nav-item nav-1-1">'+
-					  	'<a class="nav-link nav-head" href="index.html">Home</a>'+
-					  '</li>'+
-					   '<li class="nav-item nav-1-2">'+
-					  	'<a class="nav-link nav-head" href="signup.html">Sign Up</a>'+
-					  '</li>'+
-					'</ul>'+
-				'</nav>'+
-			 '</div>';
-
-		 document.getElementById("universal-header").innerHTML =
-			'<div class="container-header">'+
-			'<img class ="cs-logo" src="Logo.png" alt="cs-portal-logo">' +
-				'<h1 class="logo-header text-center">Computer Science Project Portal</h1>'+
-			'</div> '	+ '<h3 class="sub-logo-header">CS673 Team 4</h3>' ;
-
-		 document.getElementById("universal-footer").innerHTML =
-			'<div class="container text-center footer">'+
-				'<p>common footer . . .</p>'+
-			'</div>';
-
 //Validate Form
 	function validateForm() {
 		var alphaExp = /^[a-zA-Z]+$/;
