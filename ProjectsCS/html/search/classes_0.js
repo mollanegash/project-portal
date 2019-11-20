419a8e2dd3690058e2c32c29977747c261fcb806
Rattikarn <soyosss95@gmail.com>
2019-11-20 13:31:18 -0500

    add redundancie files

--- ProjectsCS/html/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['cannotperformoperationexception_7',['CannotPerformOperationException',['../class_test_form_1_1_cannot_perform_operation_exception.html',1,'TestForm']]]
-];
--- ProjectsCS/html/search/classes_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['registeration_13',['registeration',['../class_test_form_1_1registeration.html',1,'TestForm']]]
-];
