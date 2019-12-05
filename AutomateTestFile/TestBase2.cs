95be0885ccd8c9551630fd4a3477448e97c5b32f
Juan P Sanchez (JP) <juanpasanchez@JUANs-MacBook-Pro.local>
2019-12-04 20:17:59 -0500

    add autotest for failing registration

--- AutomateTestFile/TestBase2.cs
+++ AutomateTestFile/TestBase2.cs
@@ -20,9 +20,9 @@ namespace CSPortalTest
           drive.FindElement(By.Id("lastNameTextBox")).SendKeys("AutoTest Lname");
           drive.FindElement(By.Id("schoolTextBox")).SendKeys("AutoTest SchoolName");
           drive.FindElement(By.Id("DropDownList1")).SendKeys("Student");
-          drive.FindElement(By.Id("usernameTextBox")).SendKeys("#AutoTestStudent@bu.edu");
-          drive.FindElement(By.Id("passwordTextBox")).SendKeys("#AutoTestPassword");
-          drive.FindElement(By.Id("confirmPasswordTextBox")).SendKeys("#AutoTestPassword");
+          drive.FindElement(By.Id("usernameTextBox")).SendKeys("AutoTestStudent@bu.edu");
+          drive.FindElement(By.Id("passwordTextBox")).SendKeys("AutoTestPassword");
+          drive.FindElement(By.Id("confirmPasswordTextBox")).SendKeys("AutoTestPassword");
 
 
           //click to enter into the system
@@ -34,6 +34,32 @@ namespace CSPortalTest
         }
 
 
+        // Fail to Create new student account
+        [Test]
+        static void createStudentTestFail(IWebDriver driver)
+        {
+          //Enter student details
+          drive.FindElement(By.Id("firstnameTextBox")).SendKeys("FailAutoTestFname");
+          drive.FindElement(By.Id("lastNameTextBox")).SendKeys("FailAutoTestLname");
+          drive.FindElement(By.Id("schoolTextBox")).SendKeys("FailAutoTestSchoolName");
+          drive.FindElement(By.Id("DropDownList1")).SendKeys("Student");
+          drive.FindElement(By.Id("usernameTextBox")).SendKeys("FailAutoTestStudent@bu.edu");
+          drive.FindElement(By.Id("passwordTextBox")).SendKeys("AutoTestPassword");
+          drive.FindElement(By.Id("confirmPasswordTextBox")).SendKeys("AutoTestPassword2");
+
+
+          //click to enter into the system
+          driver.FindElement(By.Id("registerButton")).Click();
+
+          driver.Manage().Window.Maximize();
+          driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20); // set locatorTimeout to be 20 secound at max
+
+        }
+
+
+        // 
+
+
         // Create new faculty account
         [Test]
         static void createFacultyTest(IWebDriver driver)
@@ -43,9 +69,9 @@ namespace CSPortalTest
           drive.FindElement(By.Id("lastNameTextBox")).SendKeys("AutoTest Lname");
           drive.FindElement(By.Id("schoolTextBox")).SendKeys("AutoTest SchoolName");
           drive.FindElement(By.Id("DropDownList1")).SendKeys("Faculty");
-          drive.FindElement(By.Id("usernameTextBox")).SendKeys("#AutoTestFaculty@bu.edu");
-          drive.FindElement(By.Id("passwordTextBox")).SendKeys("#AutoTestPassword");
-          drive.FindElement(By.Id("confirmPasswordTextBox")).SendKeys("#AutoTestPassword");
+          drive.FindElement(By.Id("usernameTextBox")).SendKeys("AutoTestFaculty@bu.edu");
+          drive.FindElement(By.Id("passwordTextBox")).SendKeys("AutoTestPassword");
+          drive.FindElement(By.Id("confirmPasswordTextBox")).SendKeys("AutoTestPassword");
 
 
           //click to enter into the system
@@ -159,6 +185,8 @@ namespace CSPortalTest
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20); // set locatorTimeout to be 20 secound at max
 
         }
+
+
 
 
 
